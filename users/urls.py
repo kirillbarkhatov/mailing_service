08c81db4e61f8bfeb42b8285a1b92f93d7cc4884
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-11-18 19:27:47 +0300

    Реализован сброс пароля

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 
 from users.apps import UsersConfig
 
@@ -10,12 +10,24 @@ from . import views
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
     path(
-        "logout/", LogoutView.as_view(next_page="mailing:index"), name="logout"
+        "logout/", auth_views.LogoutView.as_view(next_page="mailing:index"), name="logout"
     ),
     path("register/", views.RegisterView.as_view(), name="register"),
     path("email-confirm/<str:token>/", views.email_verification, name="email-confirm"),
+
+    # Страница для ввода email
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="password_reset/password_reset_form.html", email_template_name="password_reset/password_reset_email.html", success_url=reverse_lazy("users:password_reset_done")), name='password_reset'),
+
+    # Уведомление об отправке email
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset/password_reset_done.html"), name='password_reset_done'),
+
+    # Ссылка из email, форма ввода нового пароля
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset/password_reset_confirm.html", success_url=reverse_lazy("users:password_reset_complete")), name='password_reset_confirm'),
+
+    # Уведомление об успешном сбросе пароля
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset/password_reset_complete.html"), name='password_reset_complete'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
